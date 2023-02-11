cfa2dd72c7df593ee8d1fc016d3e05a925493d43
timrbarrett <timrbarrett@users.noreply.github.com>
2023-02-11 12:52:32 +0000

    second attempt

--- NUS.js
+++ NUS.js
@@ -1,35 +1,14 @@
 const button = document.querySelector('#connect-button');
+button.addEventListener('click', async () => {
+  try {
+    const device = await navigator.bluetooth.requestDevice({
+      filters: [{ namePrefix: 'NUS' }],
+      optionalServices: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e']
+    });
 
-button.addEventListener('click', 90 => {
-
-	navigator.bluetooth.requestDevice({
-	  filters: [{ services: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e'] }]
-	})
-	.then(device => {
-	  console.log('Connecting to ' + device.name);
-	  return device.gatt.connect();
-	})
-	.then(server => {
-	  console.log('Getting NUS Service...');
-	  return server.getPrimaryService('6e400001-b5a3-f393-e0a9-e50e24dcca9e');
-	})
-	.then(service => {
-	  console.log('Getting NUS RX Characteristic...');
-	  return service.getCharacteristic('6e400003-b5a3-f393-e0a9-e50e24dcca9e');
-	})
-	.then(characteristic => {
-	  console.log('Start Notifying...');
-	  return characteristic.startNotifications().then(_ => {
-		console.log('Notifications started');
-		characteristic.addEventListener('characteristicvaluechanged',
-		  handleNUSData);
-	  });
-	})
-	.catch(error => { console.log(error); });
-
-]);
-
-function handleNUSData(event) {
-  let value = new TextDecoder().decode(event.target.value);
-  console.log('Received NUS data: ' + value);
-}
+    const server = await device.gatt.connect();
+    console.log('Connected to NUS device');
+  } catch (error) {
+    console.error(error);
+  }
+});
