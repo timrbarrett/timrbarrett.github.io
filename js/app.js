630cb0a05298ed92eb5c1a42b11b1a9702558695
timrbarrett <timrbarrett@users.noreply.github.com>
2023-02-19 10:51:18 +0000

    added enter at end of line

--- js/app.js
+++ js/app.js
@@ -24,13 +24,16 @@ function connectionToggle() {
 
 function sendInit() {
     if (connected) {
-        nusSendString("rdac on");
-        nusSendString("pulses on");
+        nusSendString("rdac on\r");
+        window.term_.io.println('rdac on');
+        nusSendString("pulses on\r");
+        window.term_.io.println('pulses on');
         nusSendString("utc t "
             + currentdate.getHours() + " "
             + currentdate.getMinutes() + " "
-            + currentdate.getSeconds()
+            + currentdate.getSeconds() + "\r"
         );
+        window.term_.io.println('utc t message');
     } else {
         window.term_.io.println('Must be connected to init!');
     }
