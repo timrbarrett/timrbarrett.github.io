4c45a45f68fe6d535a51fdf3de440744cd25d2c1
timrb <timrbarrett@gmail.com>
2025-04-14 16:46:19 +0100

    second attempt to confirm renderer is active

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "a8611da0edd9f89eb371f524fb38d024",
+"flutter_bootstrap.js": "adf4dce60422d84c60dd23dc80e12814",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
