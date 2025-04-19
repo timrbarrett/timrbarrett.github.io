4ce6749e6363bb83cd8dfa937ab0dba6064ec4e7
timrb <timrbarrett@gmail.com>
2025-04-19 14:08:22 +0100

    confirm PWA status.

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4098172630"
+    serviceWorkerVersion: "1758039569"
   }
 });
--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c12255cb72af0d5a2e17fbdea799222c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "7e8543f7c66778327ded5ebf5455b4e2",
+"flutter_bootstrap.js": "60b923d7e88d525eeda9d3fc04dfe579",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
