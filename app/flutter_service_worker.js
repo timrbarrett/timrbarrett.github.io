5e05163a13147316e841bd6fbfbe801647cb9d5a
timrb <timrbarrett@gmail.com>
2025-04-23 15:27:57 +0100

    Install to an icon on app bar

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "c12255cb72af0d5a2e17fbdea799222c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "3cfdadda7988461b141c5c77b0af156e",
+"flutter_bootstrap.js": "4a85f570625ec3ac3da47534b1c486a3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "17058067a850145acb9aa50f66994d1c",
 "/": "17058067a850145acb9aa50f66994d1c",
-"main.dart.js": "dac809c47dc6313c1cc8bfcf50ec155d",
+"main.dart.js": "d7f0e03e26b6ce7b634213eda23e57e8",
 "manifest.json": "ac7e55655c3ea9a125e3b1c8d0abd124",
 "version.json": "0d0986f2d8f0447d91e3b1bff5101787"};
 // The application shell files that are downloaded before a service worker can
