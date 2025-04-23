4d33ff9b5ed628928ca68c5142d0b284c92d71db
timrb <timrbarrett@gmail.com>
2025-04-23 15:01:48 +0100

    trying to update the buttons.json asset

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c12255cb72af0d5a2e17fbdea799222c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "90988658920fa67edadbb6b6cc8089d8",
+"flutter_bootstrap.js": "364a13a0531af7537ae4f4a5803cbfe1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
