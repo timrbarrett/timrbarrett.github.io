a69ff40491436012df85a11aa1214773969ee5f1
timrb <timrbarrett@gmail.com>
2025-04-25 16:24:25 +0100

    double tap clears received and portrait and landscape both fill the screen.

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "c12255cb72af0d5a2e17fbdea799222c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "1ab987a9c7815f7419d0e4df3f32086d",
+"flutter_bootstrap.js": "09294b61cf48ea42f0b10da27234ec0c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "e16e0fa1cff7f8b56577c7d0b303d6dc",
 "/": "e16e0fa1cff7f8b56577c7d0b303d6dc",
-"main.dart.js": "279c56c4754acacd4a2eba5b400744e0",
+"main.dart.js": "0b3260d398110f448feca981d5e5e731",
 "manifest.json": "2a873ac0b7203b606f0f434dd2563564",
 "version.json": "0d0986f2d8f0447d91e3b1bff5101787"};
 // The application shell files that are downloaded before a service worker can
