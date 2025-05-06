2ec9d440b77ebd1f89f84ed5fe4de30f9aafb3cb
timrb <timrbarrett@gmail.com>
2025-05-06 14:59:22 +0100

    3d point cloud working on web

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a0efc511014e3f393e9ece68b116bc13
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "bbece40596118e4c118930e9326b5e59",
+"flutter_bootstrap.js": "686c086bab65a70195e2fa359caf4671",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
