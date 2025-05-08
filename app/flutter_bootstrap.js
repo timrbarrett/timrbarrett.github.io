e7cd53a8f6043355c3055101174bf560189aa032
timrb <timrbarrett@gmail.com>
2025-05-08 14:09:32 +0100

    interactive mag reading.

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3597337400"
+    serviceWorkerVersion: "1305365807"
   }
 });
