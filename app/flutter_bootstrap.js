c4d8278c69b4c2be9a9dd653fa9083f4d7965cb4
timrb <timrbarrett@gmail.com>
2025-06-16 10:59:14 +0100

    return to basic cube

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1397618903"
+    serviceWorkerVersion: "1721672466"
   }
 });
