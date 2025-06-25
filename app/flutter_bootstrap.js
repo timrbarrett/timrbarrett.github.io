7dcadf3571a67ca3e59ca90db49fb6f13721556f
timrb <timrbarrett@gmail.com>
2025-06-25 13:07:55 +0100

    colour buttons

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1274653343"
+    serviceWorkerVersion: "2023110012"
   }
 });
