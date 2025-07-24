a3c49c9d1cddfa399a8f8432aef754f38c5082d3
timrb <timrbarrett@gmail.com>
2025-07-24 11:13:11 +0100

    second automation attempt

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -66,7 +66,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f68efd44918613c6e3b722f54ef23951
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "e644cecfeccae2b4ab4e9ca2778785d5",
+"flutter_bootstrap.js": "6227cb2811671f4b80828e9475ee9586",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
