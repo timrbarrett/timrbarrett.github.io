ff4f5ed972658338629b159b72a117d04e3e8af4
timrb <timrbarrett@gmail.com>
2025-07-27 11:14:07 +0100

    shard preferences part 03

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2224060428"
+    serviceWorkerVersion: "4005945344"
   }
 });
