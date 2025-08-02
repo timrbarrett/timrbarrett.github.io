ede20282b6f905dd16ff35caa65f79146b53bc8c
Tim Barrett <timrbarrett@gmail.com>
2025-08-02 10:31:42 +0100

    Update app from build on 2025-8-2-1031

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -55,7 +55,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5c94651d38fe2d9bf8f03b3473d9e0f4
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "e867488be419aaa30b62c742d1297d14",
+"flutter_bootstrap.js": "f9e38b16c66d718a1b7fd2c6a83fff99",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
