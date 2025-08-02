79972df508a2bef990c7dd3a8d2fb2a3c6f819f6
Tim Barrett <timrbarrett@gmail.com>
2025-08-02 13:53:43 +0100

    Update app from build on 2025-8-2-1353

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "486959264"
+    serviceWorkerVersion: "1141199945"
   }
 });
