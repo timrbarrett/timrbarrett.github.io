75f7987f1c8251189a3c050b5f202c3d38fb4c55
timrb <timrbarrett@gmail.com>
2026-04-22 11:53:28 +0100

    Update app from build on 2026-04-22-1153

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -54,7 +54,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b64e14720608e29b7da14c9234d2b912
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "09e7531a3ab396ac808613d94fd4ee64",
+"flutter_bootstrap.js": "82eaa1c901fe3994198f819044b14587",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
