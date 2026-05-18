fec9e66976bace9dacc69c5673be8ffe533860ef
timrb <timrbarrett@gmail.com>
2026-05-18 09:09:15 +0100

    Update app from build on 2026-05-18-0909

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2158806948" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2254441369" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
