eba1982b9f0c0492f4979cd7eff9c8e2efe05e31
timrb <timrbarrett@gmail.com>
2026-05-24 17:55:55 +0100

    Update app from build on 2026-05-24-1755

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -33,6 +33,6 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d937","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=0.598-29b8e29"}]};
+_flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d937","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=0.598-eec77f3"}]};
 
 _flutter.loader.load();
