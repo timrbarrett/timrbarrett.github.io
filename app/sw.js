4eef22e2eee43cbbd6cc6409f77fbe55a0bf4452
timrb <timrbarrett@gmail.com>
2026-09-03 12:14:29 +0100

    Update app from build on 2026-09-03-1214

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for offline functionality
-const CACHE_NAME = 'limbstim-app-0.598-fce2e37-20260903-111441';
+const CACHE_NAME = 'limbstim-app-0.598-659ff79-20260903-121344';
 const BASE_HREF = '/app/';
 const OFFLINE_URL = BASE_HREF + 'offline.html';
 
